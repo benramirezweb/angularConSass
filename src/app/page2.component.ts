8da1e04b0f87152d99cc18b78cfea6b297955091
benramirezweb <benjamin.dg@gmail.com>
2018-10-11 14:15:10 -0500

    Primer commit

---
+++ src/app/page2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page2',
+  templateUrl: './page2.component.html',
+  styles: []
+})
+export class Page2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
